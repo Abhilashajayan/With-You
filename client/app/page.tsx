565f8b7049eb74af710c4e6c51c22b3ede79c965
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2024-02-17 00:33:58 +0530

    the signup added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,1 @@
-import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <div className="h-screen">
-           <UserButton afterSignOutUrl="/"/>
-       </div>
-    </main>
-  );
-}
