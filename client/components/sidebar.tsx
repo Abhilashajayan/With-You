7a95ae02c60b1e857997ed297fd05f417ca12a4b
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2024-02-21 00:23:08 +0530

    the admin dashboard front end done

--- client/components/sidebar.tsx
+++ client/components/sidebar.tsx
@@ -1,65 +1,168 @@
-"use client"
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Layout, Button, Avatar, Menu, Dropdown, theme } from "antd";
 
-const Sidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = React.useState(true);
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+  PaperClipOutlined,
+  RiseOutlined,
+} from "@ant-design/icons";
+import { default as Bill } from "@/app/admin/billing/page";
+import Report from "@/app/admin/reports/page";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faBell,
+  faCircleQuestion,
+  faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+const { Header, Sider, Content } = Layout;
+
+function AdminSideBar() {
+  const [collapsed, setCollapsed] = useState(false);
+  const router = useRouter();
+
+  const [navbarOpen, setNavbarOpen] = useState<Boolean>(false);
+  useEffect(() => {
+    setNavbarOpen(false);
+  });
+
+  const [selectedMenuKey, setSelectedMenuKey] = useState<string | null>("1");
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  const handleMenuClick = ({ key }: { key: React.Key }) => {
+    console.log(key);
+    setSelectedMenuKey(key.toString());
   };
 
   return (
-    <div className="flex">
-      {/* Hamburger icon for small screens */}
-      <div className="md:hidden">
-        <button
-          className="text-gray-500 p-4 focus:outline-none"
-          onClick={toggleSidebar}
-        >
-          {isSidebarOpen ? (
-            <FontAwesomeIcon icon={faTimes} className="h-6 w-6" />
-          ) : (
-            <FontAwesomeIcon icon={faBars} className="h-6 w-6" />
-          )}
-        </button>
-      </div>
-      <div
-        className={`flex-none md:w-64 bg-white border-r ${isSidebarOpen ? 'block' : 'hidden'}`}
+    <Layout>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{ background: colorBgContainer }}
       >
-        <div className="p-4 h-screen ">
-          <h1 className="text-xl font-semibold mb-4">Admin Dashboard</h1>
-          <ul>
-            <li className="mb-2">
-              <Link href="/admin/dashboard" className='text-blue-500 hover:text-blue-600'>
-                Dashboard
-              </Link>
-            </li>
-            <li className="mb-2">
-              <Link href="/users" className='text-blue-500 hover:text-blue-600'>
-                Users
-              </Link>
-            </li>
-            <li className="mb-2">
-              <Link href="/products" className='text-blue-500 hover:text-blue-600'>
-               Products
-              </Link>
-            </li>
-          </ul>
+        <div className="h-16 flex justify-center text-lg font-bold ">
+          <h1>Admin</h1>
         </div>
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <div className="p-4">
-          <h1 className="text-2xl font-semibold mb-4">Welcome to the Admin Dashboard</h1>
-          {/* Add your main content here */}
+        <Menu
+          theme="light"
+          mode="inline"
+          onClick={handleMenuClick}
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "Users",
+            },
+            {
+              key: "2",
+              icon: <RiseOutlined />,
+              label: "Orders",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <div className="relative flex flex-wrap  items-center justify-end px-2 py-3 bg-white ">
+          <div className="container px-3 mx-auto flex flex-wrap items-center justify-evenly">
+            <div className="w-full relative flex  justify-evenly lg:w-auto lg:static lg:block lg:justify-start">
+              <Button
+                type="text"
+                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                onClick={() => setCollapsed(!collapsed)}
+                className="text-2xl w-5 h-5"
+              />
+              <a
+                className="ml-14 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-textgrey"
+                href="#pablo"
+              >
+                Bea
+              </a>
+              <button
+                className="text-textgrey cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                type="button"
+                onClick={() => setNavbarOpen(!navbarOpen)}
+              >
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div
+              className={
+                "lg:flex flex-grow items-center" +
+                (navbarOpen ? " flex" : " hidden")
+              }
+              id="example-navbar-danger"
+            >
+              <ul className="flex flex-col lg:flex-row list-none lg:ml-auto p-2">
+               
+                <li className="nav-item">
+                  <FontAwesomeIcon
+                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-textgrey hover:opacity-75"
+                    icon={faEnvelope}
+                  />
+                </li>
+                <li className="nav-item">
+                  <FontAwesomeIcon
+                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-textgrey hover:opacity-75"
+                    icon={faCircleQuestion}
+                  />
+                </li>
+                
+                <li className="nav-item">
+                  <button className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75">
+                    Logout
+                  </button>
+                </li>
+                 
+                <li className="nav-item">
+                  <div className=" h-8 w-8 rounded-3xl">
+                    <Link href={`/adminprofile`}>
+                      <img
+                        src="https://cdn3.iconfinder.com/data/icons/user-group-black/100/user-process-512.png"
+                        className="h-8 w-8 rounded-3xl"
+                        width={500}
+                        height={500}
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+        <Content className="mt-6 mr-4 ml-6 p-10  h-screen min-h-min">
+          {selectedMenuKey === "1" && <Bill />}
+          {selectedMenuKey === "2" && <Report />}
+        </Content>
+      </Layout>
+    </Layout>
   );
-};
+}
 
-export default Sidebar;
+export default AdminSideBar;
