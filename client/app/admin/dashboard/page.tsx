f2561a9c451db32677f30f6985b1aff648151e5e
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2024-02-22 00:25:24 +0530

    the admin dashbaord the apexchart is added

--- client/app/admin/dashboard/page.tsx
+++ client/app/admin/dashboard/page.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import Layouts from "@/components/Layout";
 import { ExampleChart } from "@/components/Chart";
-
+import { FaMoneyBill, FaUsers, FaDollarSign, FaUser } from "react-icons/fa";
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Layouts>
-        <div className="bg-gray-100">
+        <div className="">
           <h1 className="text-gray-500 p-5 ">Dashboard</h1>
           <div className="flex space-x-8 p-5">
-            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px]  rounded-lg">
-
+            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px] rounded-lg flex items-center justify-center">
+              <FaMoneyBill className="mr-2" />
+              Today's Amount
             </div>
-            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px]  rounded-lg">
-
+            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px] rounded-lg flex items-center justify-center">
+              <FaUsers className="mr-2" />
+              Total Users
             </div>
-            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px]  rounded-lg">
-
+            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px] rounded-lg flex items-center justify-center">
+              <FaDollarSign className="mr-2" />
+              Total Amount
             </div>
-            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px]  rounded-lg">
-
+            <div className="p-6 mt-7 border border-gray-500 w-[200px] h-[70px] rounded-lg flex items-center justify-center">
+              <FaUser className="mr-2" />
+              Total Users
             </div>
           </div>
 
-          <div className="bg-gray-100 mt-7 flex justify-center p-5">
+          <div className=" mt-7 flex justify-center p-5">
             <ExampleChart />
           </div>
         </div>
