61e8a76a2dcd12a4336d4144a7654021f21614b9
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2024-04-25 12:43:15 +0530

    fix(core) : fix in the video call

--- client/app/(root)/Message/videoCall/page.tsx
+++ client/app/(root)/Message/videoCall/page.tsx
@@ -21,6 +21,7 @@ function VideoCallPage() {
   const [selectedUser, setSelectedUser] = useState<string | null>(null);
   const [outgoingCall, setOutgoingCall] = useState<boolean>(false);
   const [incomingCall, setIncomingCall] = useState<boolean>(false);
+  const [name, setName] = useState<string | null>(null);
   const router = useRouter();
   const searchParams = useSearchParams();
   const otherUserId = searchParams.get("User");
@@ -33,11 +34,12 @@ function VideoCallPage() {
       setIsConnected(true);
     });
 
-    socket.on("call received", async (callerId: string) => {
-      console.log("Call received from remote user:", callerId);
+    socket.on("call received", async (callerId: string, username: string) => {
+      console.log("Call received from remote user:", callerId, username);
       console.log("Setting incoming call to true...");
       setIncomingCall(true);
       setSelectedUser(callerId);
+      setName(username);
     });
 
     socket.on("cancel call", () => {
@@ -78,7 +80,11 @@ function VideoCallPage() {
       setOutgoingCall(true);
       setIsCalling(true);
       setSelectedUser(otherUserId);
-      socket.emit("call", { callerId: user._id, recipientId: otherUserId });
+      socket.emit("call", {
+        callerId: user._id,
+        recipientId: otherUserId,
+        username: user.username,
+      });
       return stream;
     } catch (error) {
       console.error("Error starting call:", error);
@@ -93,7 +99,10 @@ function VideoCallPage() {
     setOutgoingCall(false);
     setIncomingCall(false);
     setSelectedUser(null);
-    socket.emit("cancel call", { callerId: user._id, recipientId: otherUserId });
+    socket.emit("cancel call", {
+      callerId: user._id,
+      recipientId: otherUserId,
+    });
   };
 
   const handleAcceptCall = async () => {
@@ -134,25 +143,29 @@ function VideoCallPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-500 flex flex-col justify-center items-center">
-      <div className="container relative w-full max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-        {/* <h1 className="py-6 text-3xl font-bold text-gray-800 text-center">Video Call</h1> */}
+    <div className="h-full  inset-0 -z-10 bg-black bg-[radial-gradient(#ff0000_1px,transparent_1px)] [background-size:20px_20px] lg:rounded-r-none lg:shadow-2xl bg-white opacity-90 mx-6 lg:mx-0 mb-8 lg:mb-0 flex flex-col justify-center ">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+      <h1 className="py-6 text-3xl font-bold text-gray-800 text-center ">
+        {name}
+      </h1>
+      <div className="container relative w-full max-w-md mx-auto   rounded-lg overflow-hidden">
         {isConnected ? (
           <>
             {!isCalling && !incomingCall ? (
-              <button className="btn start-call bg-green-500 hover:bg-green-600 text-white py-3 px-6 rounded-full mb-4 mx-auto block" onClick={handleStartCall}>
+              <button
+                className="btn start-call bg-green-500 hover:bg-green-600 text-white py-3 px-6 rounded-full mb-4 mx-auto block"
+                onClick={handleStartCall}
+              >
                 <CallIcon className="mr-2" />
                 Start Call
               </button>
             ) : (
               <>
-               
                 {isCallAccepted ? (
                   <VideoCall
                     localStream={localStream}
                     remoteStream={remoteStream}
                     onCallAccepted={handleCallAccepted}
-                    
                   />
                 ) : null}
               </>
@@ -163,30 +176,40 @@ function VideoCallPage() {
         )}
       </div>
       {incomingCall && !isCallAccepted ? (
-                  <div className="call-options flex items-center justify-center">
-                    <p className="text-lg font-semibold text-gray-800 mr-4">Incoming Call...</p>
-                    <button
-                      className="btn accept bg-green-500 hover:bg-green-600 text-white py-3 px-6 rounded-full transition duration-300 ease-in-out"
-                      onClick={handleAcceptCall}
-                    >
-                      Accept
-                    </button>
-                    <button
-                      className="btn reject bg-red-500 hover:bg-red-600 text-white py-3 px-6 rounded-full ml-2 transition duration-300 ease-in-out"
-                      onClick={handleRejectCall}
-                    >
-                      Reject
-                    </button>
-                  </div>
-                ) : (
-                  <div className="call-options flex items-center justify-center">
-                    <button className="bg-red-500 hover:bg-red-600 text-white py-3 px-6 rounded-full ml-2 transition duration-300 ease-in-out" onClick={handleEndCall}>
-                      End Call
-                    </button>
-                  </div>
-                )}
+        <div className="call-options flex items-center justify-center">
+          <p className="text-lg font-semibold text-gray-800 mr-4">
+            Incoming Call...
+          </p>
+          <button
+            className="btn accept bg-green-500 hover:bg-green-600 text-white py-3 px-6 rounded-full transition duration-300 ease-in-out"
+            onClick={handleAcceptCall}
+          >
+            Accept
+          </button>
+          <button
+            className="btn reject bg-red-500 hover:bg-red-600 text-white py-3 px-6 rounded-full ml-2 transition duration-300 ease-in-out"
+            onClick={handleRejectCall}
+          >
+            Reject
+          </button>
+        </div>
+      ) : null}
+      {isCallAccepted || isCalling ? (
+        <div className="call-options flex items-center justify-center">
+          <button
+            className="bg-red-500 hover:bg-red-600 text-white py-3 px-6 rounded-full ml-2 transition duration-300 ease-in-out"
+            onClick={handleEndCall}
+          >
+            End Call
+          </button>
+        </div>
+      ) : null}
 
-                       
+      {isCalling && !isCallAccepted ? (
+        <div className="call-options flex items-center justify-center">
+          <p className="text-lg font-semibold text-gray-800 mr-4">Ringing</p>
+        </div>
+      ) : null}
     </div>
   );
 }
